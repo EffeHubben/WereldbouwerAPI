82b3e61b70c1b72c1d4d1524875f6ed3d9018630
KyanPat <kyantsoi1@gmail.com>
2025-02-11 13:56:19 +0100

    wereldbouwer aangepast

---
+++ WereldbouwerAPI/Controllers/WereldBouwerController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WereldbouwerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WereldBouwerController : ControllerBase
+    {
+
+        private List<WereldBouwer> _wereldBouwers = new List<WereldBouwer>();
+        private readonly ILogger<WereldBouwerController> _logger;
+
+        public WereldBouwerController(ILogger<WereldBouwerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWereldBouwer")]
+        public IEnumerable<WereldBouwer> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WereldBouwer
+            {
+                name = jeff,
+                maxLength = Random.Shared.Next(-20, 55),
+                maxHeight = Random.Shared.Next(1, 100) // Assuming Summaries is an array of integers
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "PostWereldBouwer")]
+        public ActionResult<WereldBouwer> Post(WereldBouwer wereldBouwer)
+        {
+            _wereldBouwers.Add(wereldBouwer);
+            return CreatedAtRoute("GetWereldBouwer", new { }, wereldBouwer);
+        }
+
+    }
+}
---
+++ WereldbouwerAPI/WereldBouwer.cs.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WereldbouwerAPI
+{
+    public class WereldBouwer
+    {
+        public string name;
+        public int maxLength;
+        public int maxHeight;
+    }
+}
