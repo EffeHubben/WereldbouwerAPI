fef6da5425323c69d0d58f92228ec9bf0676ddce
KyanPat <kyantsoi1@gmail.com>
2025-02-22 11:32:33 +0100

    e

--- WereldBouwerTests/obj/Debug/net8.0/WereldBouwerTests.AssemblyInfo.cs
+++ WereldBouwerTests/obj/Debug/net8.0/WereldBouwerTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WereldBouwerTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec56fc145503b0445c9235e2db284455fa086531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c0a192d3b42cbcaafa39e825dc83c26eeab056")]
 [assembly: System.Reflection.AssemblyProductAttribute("WereldBouwerTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WereldBouwerTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
